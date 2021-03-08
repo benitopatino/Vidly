06bca998169b7310d2ba5b40b4d48a41da48b093
Benito Patino <contact@benitopatino.com>
2021-03-07 16:15:34 -0800

    Created details page for Movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -50,6 +50,16 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
         public ActionResult Edit(int id)
         {
             return Content("id: " + id);
